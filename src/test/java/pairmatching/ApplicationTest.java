d799d89bd95f1f721ff1abe575b95a4eb3c50a8a
ykmxxi <ykmxxi97@gmail.com>
2024-12-10 10:53:11 +0900

    build: java, gradle, mission-utils 버전 업그레이드

--- src/test/java/pairmatching/ApplicationTest.java
+++ src/test/java/pairmatching/ApplicationTest.java
@@ -4,10 +4,11 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertShuffleTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
-import pairmatching.Application;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
 
 class ApplicationTest extends NsTest {
 
@@ -16,21 +17,21 @@ class ApplicationTest extends NsTest {
     @Test
     void 짝수_인원_페어_매칭() {
         assertShuffleTest(
-            () -> {
-                run("1", "백엔드, 레벨1, 자동차경주", "Q");
-                assertThat(output()).contains("태웅 : 백호", "치수 : 태섭");
-            },
-            Arrays.asList("태웅", "백호", "치수", "태섭")
+                () -> {
+                    run("1", "백엔드, 레벨1, 자동차경주", "Q");
+                    assertThat(output()).contains("태웅 : 백호", "치수 : 태섭");
+                },
+                Arrays.asList("태웅", "백호", "치수", "태섭")
         );
     }
 
     @Test
     void 없는_미션에_대한_예외_처리() {
         assertSimpleTest(
-            () -> {
-                runException("1", "백엔드, 레벨1, 오징어게임");
-                assertThat(output()).contains(ERROR_MESSAGE);
-            }
+                () -> {
+                    runException("1", "백엔드, 레벨1, 오징어게임");
+                    assertThat(output()).contains(ERROR_MESSAGE);
+                }
         );
     }
 
@@ -38,4 +39,5 @@ class ApplicationTest extends NsTest {
     public void runMain() {
         Application.main(new String[]{});
     }
+
 }
